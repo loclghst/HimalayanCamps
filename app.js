b51843f852b89552e5556ec93288b2d1d1aac3ca
Manish Kumar Sharma <loclghst@gmail.com>
2018-10-11 20:22:41 +0530

    Add the /campgrounds route

--- app.js
+++ app.js
@@ -7,6 +7,22 @@ app.get('/', (req,res) =>{
 	res.render('landing');
 });
 
+app.get("/campgrounds", function(req, res){
+    var campgrounds = [
+        {name: "Magic Meera, Rasol", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
+        {name: "Shangarh", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
+        {name: "Chalal, Kasol", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"},
+        {name: "Magic Meera, Rasol", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
+        {name: "Shangarh", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
+        {name: "Chalal, Kasol", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"},
+        {name: "Magic Meera, Rasol", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
+        {name: "Shangarh", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
+        {name: "Chalal, Kasol", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"}
+];
+    
+    res.render("campgrounds",{campgrounds:campgrounds});
+});
+
 app.listen(9999, process.env.IP, function(){
 	console.log('Server has started');
 })
