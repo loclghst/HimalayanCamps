8376ab8ee6752ca9959dd094ef7d28d7d66516a6
Manish Kumar Sharma <loclghst@gmail.com>
2018-10-12 03:41:51 +0530

    Add data persistence using Mondo db

--- app.js
+++ app.js
@@ -1,29 +1,40 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/yelp_camp',{useNewUrlParser: true});
+
 
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.set('view engine', 'ejs');
 
-let campgrounds = [
-        {name: "Magic Meera, Rasol", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
-        {name: "Shangarh", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
-        {name: "Chalal, Kasol", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"},
-        {name: "Magic Meera, Rasol", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
-        {name: "Shangarh", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
-        {name: "Chalal, Kasol", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"},
-        {name: "Magic Meera, Rasol", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
-        {name: "Shangarh", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
-        {name: "Chalal, Kasol", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"}
-];
+//Schema setup
+
+const campgroundSchema = new mongoose.Schema({
+	name: String,
+	image: String
+});
+
+const Campground = mongoose.model('Campground', campgroundSchema);
+
 
 app.get('/', (req,res) =>{
 	res.render('landing');
 });
 
 app.get("/campgrounds", (req, res)=>{
-    res.render("campgrounds",{campgrounds:campgrounds});
+	//Get all campgrounds
+	Campground.find({}, (err,allcampgrounds)=>{
+		if(err)
+			console.error(err);
+		else{
+			res.render("campgrounds",{campgrounds:allcampgrounds});
+		}
+
+	});
+		
 });
 
 app.get('/campgrounds/new', (req,res) =>{
