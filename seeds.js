ce03b47b28da76a6397b101cad71af3ee73e9aef
Manish Kumar Sharma <loclghst@gmail.com>
2018-10-12 06:18:14 +0530

    Add seed data to seeds.js file

--- seeds.js
+++ seeds.js
@@ -1,12 +1,38 @@
 const mongoose = require('mongoose');
-const Campground = require('./models/campground');
+const Campground = require('./models/campgrounds');
+const Comment   = require("./models/comment");
+
+const data = [
+    {
+        name: "Magic Meera , Rasol", 
+        image: "https://farm4.staticflickr.com/3795/10131087094_c1c0a1c859.jpg",
+        description: "blah blah blah"
+    },
+    {
+        name: "Shangarh, Kullu", 
+        image: "https://farm4.staticflickr.com/3859/15123592300_6eecab209b.jpg",
+        description: "blah blah blah"
+    },
+    {
+        name: "Katagla, Parvati Valley", 
+        image: "https://farm1.staticflickr.com/189/493046463_841a18169e.jpg",
+        description: "blah blah blah"
+    }
+]
 
 function seedDB(){
+
+	//Remove all campgrounds
+
 	Campground.remove({}, (err) =>{
 		if(err)
 			console.error(err);
 		console.log('Remoevd Campgrounds');
 	});
+
+	//Add a few campgrounds
+
+	//Add a few comments
 }
 
 module.exports = seedDB;
