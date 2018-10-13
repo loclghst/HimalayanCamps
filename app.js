c7246a5cdc62abb28620d2e585416350dcc479f4
Manish Kumar Sharma <loclghst@gmail.com>
2018-10-13 20:28:54 +0530

    Install flash connect and configure it to use flash messages in our app

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const User = require('./models/user');
 const methodOverride = require('method-override');
+const flash = require('connect-flash');
 
 //requiring routes
 
@@ -25,6 +26,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + "/public"));
 app.use(methodOverride('_method'));
+app.use(flash());
 //seedDB(); //seed the db
 
 
@@ -42,6 +44,8 @@ passport.deserializeUser(User.deserializeUser());
 
 app.use(function(req, res, next){
    res.locals.currentUser = req.user;
+   res.locals.error = req.flash('error');
+   res.locals.success = req.flash('success');
    next();
 });
 
