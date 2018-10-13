c46730f8c54a37b56094845001822a988f6dfc1a
Manish Kumar Sharma <loclghst@gmail.com>
2018-10-13 14:57:40 +0530

    Add Auth routes

--- app.js
+++ app.js
@@ -130,6 +130,28 @@ app.post("/campgrounds/:id/comments", function(req, res){
 });
 
 
+//  ===========
+// AUTH ROUTES
+//  ===========
+
+// show register form
+app.get("/register", function(req, res){
+   res.render("register"); 
+});
+//handle sign up logic
+app.post("/register", function(req, res){
+    var newUser = new User({username: req.body.username});
+    User.register(newUser, req.body.password, function(err, user){
+        if(err){
+            console.log(err);
+            return res.render("register");
+        }
+        passport.authenticate("local")(req, res, function(){
+           res.redirect("/campgrounds"); 
+        });
+    });
+});
+ 
 
 
 
