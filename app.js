51972672c2051860da79e0a68eb1845694f7114d
Manish Kumar Sharma <loclghst@gmail.com>
2018-10-13 14:54:51 +0530

    Add passport configuration

--- app.js
+++ app.js
@@ -22,6 +22,21 @@ app.set('view engine', 'ejs');
 app.use(express.static(__dirname + "/public"));
 seedDB();
 
+
+// PASSPORT CONFIGURATION
+app.use(require("express-session")({
+    secret: "Once again Rusty wins cutest dog!",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+
 app.get('/', (req,res) =>{
 	res.render('landing');
 });
