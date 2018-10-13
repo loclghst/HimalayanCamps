1e62a13d2e7dfa67afc9d04654b3dd83f3089b64
Manish Kumar Sharma <loclghst@gmail.com>
2018-10-13 18:17:10 +0530

    Add route to edit a comment

--- routes/comments.js
+++ routes/comments.js
@@ -51,4 +51,17 @@ router.post("/", isLoggedIn, function(req, res){
    });
 });
 
+
+// COMMENT EDIT ROUTE
+router.get("/:comment_id/edit", middleware.checkCommentOwnership, function(req, res){
+   Comment.findById(req.params.comment_id, function(err, foundComment){
+      if(err){
+          res.redirect("back");
+      } else {
+        res.render("comments/edit", {campground_id: req.params.id, comment: foundComment});
+      }
+   });
+});
+
+
 module.exports = router;
